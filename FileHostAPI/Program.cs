aee3446bd760f287e529cb68af8c6a0a86c2490d
Sepehr Pakbaz <pakbaz@live.com>
2023-04-19 15:51:30 -0500

    removed extra stuff from api

--- FileHostAPI/Program.cs
+++ FileHostAPI/Program.cs
@@ -1,29 +1,13 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
 
 app.MapGet("/download", () =>
 {
     var mimeType = "application/pdf";
     var path = Environment.CurrentDirectory + @"/certs.pdf";
     return Results.File(path, contentType: mimeType);
-})
-.WithName("download")
-.WithOpenApi();
+});
 
 app.Run();
 
